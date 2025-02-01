3d21f7a1da03a2c03d81eee3c36dd8c22dcda6b8
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2025-02-01 01:09:57 +0000

    Added function for linking configs and step counter
    Added a function to link config folders.
    
    Added a steps counter to know how much is left of the setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,62 @@
 #!/bin/bash
 
-echo "Linking Hyprland setup"
+function link_config_folder {
+  rm -rf ~/.config/$1
+  ln -s ~/dotfiles/$1 ~/.config/$1
+}
 
-rm -r ~/.config/hypr # Removing any default configs
-ln -s ~/dotfiles/hypr ~/.config/hypr # Linking the dotfiles config
+total_steps=6
+step=0
+
+echo "[$step/$total_steps]Beginning environment setup..."
+
+step=step+1
+
+sleep 3
+
+echo "[$step/$total_steps]Installing prerequisite..."
+
+sudo pacman --noconfirm -Syu git base-devel
+
+step=step+1
+
+echo "[$step/$total_steps]Installing the font..."
+
+sudo pacman --noconfirm -Syu ttf-jetbrains-mono-nerd
+
+step=step+1
+
+echo "[$step/$total_steps]Setting up Hyprland..."
+
+link_config_folder "hypr"
+
+hyprctl reload # Reload Hyprland
+
+step=step+1
+echo "[$step/$total_steps]Installing yay..."
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+cd ..
+
+sudo rm -r yay
+
+step=step+1
+
+echo "[$step/$total_steps]Installing eww..."
+
+yay --noconfirm -Syu eww
+
+link_config_folder "eww"
+
+step=step+1
+
+echo "[$step/$total_steps]Installing neovim..."
+
+sudo pacman --noconfirm -Syu neovim
+
+link_config_folder "nvim"
