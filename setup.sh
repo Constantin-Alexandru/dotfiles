76ee262e45589c7f10018e37ffb123dc38fbd1e6
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2025-02-01 16:03:55 +0000

    Added kitty to setup & made yay install as noconfirm

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ function link_config_folder {
   ln -s ~/dotfiles/$1 ~/.config/$1
 }
 
-total_steps=7
+total_steps=8
 step=0
 
 echo "[$step/$total_steps]Beginning environment setup..."
 
-step=step+1
+step=$((step + 1)) 
 
 sleep 3
 
@@ -18,13 +18,25 @@ echo "[$step/$total_steps]Installing prerequisite..."
 
 sudo pacman --noconfirm -Syu git base-devel
 
-step=step+1
+step=$((step + 1)) 
+
+echo "[$step/$total_steps]Setting up kitty..."
+
+git clone https://github.com/dracula/kitty.git dracula-kitty
+
+cp dracula-kitty/dracula.conf dracula-kitty/diff.conf ~/.config/kitty/
+
+sudo rm -r dracula-kitty
+
+link_config_folder "kitty"
+
+step=$((step + 1)) 
 
 echo "[$step/$total_steps]Installing the font..."
 
 sudo pacman --noconfirm -Syu ttf-jetbrains-mono-nerd
 
-step=step+1
+step=$((step + 1)) 
 
 echo "[$step/$total_steps]Setting up Hyprland..."
 
@@ -32,20 +44,21 @@ link_config_folder "hypr"
 
 hyprctl reload # Reload Hyprland
 
-step=step+1
+step=$((step + 1)) 
+
 echo "[$step/$total_steps]Installing yay..."
 
 git clone https://aur.archlinux.org/yay.git
 
 cd yay
 
-makepkg -si
+makepkg -si --noconfirm
 
 cd ..
 
 sudo rm -r yay
 
-step=step+1
+step=$((step + 1)) 
 
 echo "[$step/$total_steps]Installing eww..."
 
@@ -53,7 +66,7 @@ yay --noconfirm -Syu eww
 
 link_config_folder "eww"
 
-step=step+1
+step=$((step + 1)) 
 
 echo "[$step/$total_steps]Installing neovim..."
 
@@ -66,3 +79,5 @@ echo "[$step/$total_steps]Installing zathura..."
 sudo pacman --noconfirm -Syu zathura zathura-pdf-mupdf
 
 link_config_folder "zathura"
+
+step=$((step + 1)) 
