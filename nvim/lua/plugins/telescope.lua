c8bf27427c5d7aab4c90e63702282d2a6f47b081
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2025-02-11 00:28:07 +0000

    NVIM telescope added extensions
    Added extensions for emojis, http code, file browser

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,15 +4,28 @@ return {
     build = "make"
   },
   {
+    'barrett-ruth/http-codes.nvim',
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {},
+    config = function()
+      require "helpers.mapping".set("n", "<leader>hc", "<cmd>HTTPCodes<CR>", "HTTP Codes")
+    end
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "BurntSushi/ripgrep",
-      "nvim-telescope/telescope-fzf-native.nvim"
+      "nvim-telescope/telescope-fzf-native.nvim",
+      "xiyaowong/telescope-emoji.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
     },
     opts = {},
     config = function()
       require "telescope".load_extension("fzf")
+      require "telescope".load_extension("emoji")
+      require "telescope".load_extension("file_browser")
+
 
       local set = require "helpers.mapping".set
       local builtin = require "telescope.builtin" -- Allows the calling of the different telescope pickers
@@ -22,6 +35,8 @@ return {
       set("n", "<leader>fg", builtin.live_grep, "Telescope live grep")
       set("n", "<leader>fb", builtin.buffers, "Telescope buffers")
       set("n", "<leader>fh", builtin.help_tags, "Telescope help tags")
+      set("n", "<leader>fe", ":Telescope emoji<CR>", "Telescope emoji list")
+      set("n", "<leader>fb", ":Telescope file_browser<CR>", "Telescope file browser")
     end
 
   }
