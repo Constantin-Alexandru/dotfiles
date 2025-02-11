af679077dc79f621a5f582daa19b4eae988b4e6d
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2025-02-11 00:28:31 +0000

    NVIM added new languages to the installation
    Added syntax download for bash, yuck and cmake

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,12 +4,12 @@ return {
     build = ":TSUpdate",
     config = function()
       require "nvim-treesitter.configs".setup({
-	ensure_installed = { "vim", "vimdoc", "lua", "c", "cpp" },
-	auto_install = false,
-	highlight = {
-	  enable = true,
-	  additional_vim_regex_highlighting = false,
-	}
+        ensure_installed = { "vim", "vimdoc", "lua", "c", "cpp", "bash", "yuck", "cmake" },
+        auto_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        }
       })
     end
   }
